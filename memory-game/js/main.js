e03e1716dafd7288b3fde3c4939464f5501c795b
Lek Hwee Meng <lekhweemeng@Leks-MacBook-Pro.local>
2020-03-10 19:14:11 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,14 @@
-let cardOne = "king";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "queen";
-console.log("User Flipped " + cardThree);
-console.log("User Flipped " + cardOne);
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+var cardOne = cards[2];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log("user flipped " + cardsInPlay[0]);
+console.log("user flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2)
+	{if (cardsInPlay[0] === cardsInPlay[1])
+		{alert("You found a mathc!");}
+	else {alert("Sorry, try again.")}
+	}
