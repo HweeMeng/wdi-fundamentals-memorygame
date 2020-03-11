73826a5a7d3fa97217e561b566258a4191fd4c97
Lek Hwee Meng <lekhweemeng@Leks-MacBook-Pro.local>
2020-03-11 13:49:31 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,14 +1,32 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-var cardOne = cards[2];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("user flipped " + cardsInPlay[0]);
-console.log("user flipped " + cardsInPlay[1]);
 
-if (cardsInPlay.length === 2)
-	{if (cardsInPlay[0] === cardsInPlay[1])
-		{alert("You found a mathc!");}
-	else {alert("Sorry, try again.")}
-	}
+function checkForMatch (){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+
+function flipCard (cardId){
+
+console.log("User flipped " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+};
+
+//if (cardsInPlay.length === 2)
+//	{if (cardsInPlay[0] === cardsInPlay[1])
+//		{alert("You found a match!");}
+//	else {alert("Sorry, try again.");}
+//	}
+
+}
+
+flipCard(0);
+flipCard(2);
